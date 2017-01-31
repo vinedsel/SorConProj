d86d13ae2e723d525653aef2ac4a76072f4ddeb3
Raymond Gregory <vinedsel@gmail.com>
2017-01-31 01:00:23 -0600

    Still can't get JS file to work.

--- SorConJS.js
+++ SorConJS.js
@@ -1,1 +1,7 @@
-document.getElementsByName("shoplist").style.textDecoration = "line-through";
+var el = document.getElementByName("shoplist").checked;
+var i;
+for (var i = 0; i < el.length; i++) {
+    if (el[i].checked;) {
+        el.style.textDecoration = "line-through";
+    }
+}
